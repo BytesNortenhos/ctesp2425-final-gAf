1b0a7953696229717ec919c5bdbd7420d2b5bb2a
Roberto Valente <1231555@isep.ipp.pt>
2025-01-31 19:41:23 +0000

    Adicionado: Models Base

---
+++ ctesp2425-final-gAf/Reservation.cs
@@ -0,0 +1,14 @@
+namespace ctesp2425_final_gAf
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDateTime { get; set; }
+        public Table Table { get; set; }
+        public int NumberOfPeople { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int StatusReservation { get; set; }
+
+    }
+}
---
+++ ctesp2425-final-gAf/Table.cs
@@ -0,0 +1,8 @@
+namespace ctesp2425_final_gAf
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public int StatusTable { get; set; }
+    }
+}
